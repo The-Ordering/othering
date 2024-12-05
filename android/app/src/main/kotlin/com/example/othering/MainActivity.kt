aa512e7452f52323dce9416ddad07e7e5e4a4301
sophat04 <sophatsorm2023@gmail.com>
2024-12-05 12:43:45 +0700

    first push

---
+++ android/app/src/main/kotlin/com/example/othering/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.othering
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/models/product_model.dart
@@ -0,0 +1,15 @@
+class ProductModel {
+  int id;
+  String name;
+  String description;
+  double price;
+  String image;
+
+  ProductModel({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.image,
+  });
+}
